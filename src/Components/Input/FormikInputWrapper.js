78eb8c8ed105fe36c68fba812c285307c8b4571b
stinodes <stijn.tytgat@ixor.be>
2018-09-11 09:05:47 +0200

    * Improved formik wrapper: Added setFieldTouched prop

--- src/Components/Input/FormikInputWrapper.js
+++ src/Components/Input/FormikInputWrapper.js
@@ -6,6 +6,7 @@ import { StyledTextInput } from './StyledInput';
 export type EnhancedHandlers = {
   onChange: (name: string, value: string) => any,
   onChangeText?: (value: string) => any,
+  setFieldTouched?: (name: string, touched: boolean) => any,
   onBlur?: () => any,
   onFocus?: () => any,
   value: string,
@@ -15,12 +16,24 @@ export type EnhancedHandlers = {
 export const FormikInputWrapper = (
   TextInputComponent: React.ComponentType<*>,
 ) => {
-  return ({ onChange, onChangeText, name, ...props }: EnhancedHandlers & *) => (
+  return ({
+    onChange,
+    onChangeText,
+    name,
+    setFieldTouched,
+    setFieldValue,
+    onBlur,
+    ...props
+  }: EnhancedHandlers & *) => (
     <TextInputComponent
       {...props}
       onChangeText={(value: string) => {
         onChange(name, value);
         typeof onChangeText === 'function' && onChangeText(value);
+      }}
+      onBlur={() => {
+        typeof setFieldTouched === 'function' && setFieldTouched(name, true);
+        typeof onBlur === 'function' && onBlur();
       }}
     />
   );
